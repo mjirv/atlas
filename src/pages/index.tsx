7c9eda2c91bb71a421986a798ed6af95d7eaab92
Michael Irvine <michael.j.irvine@gmail.com>
2022-09-07 20:05:00 -0400

    chore: updates eslintrc/prettierrc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Head from 'next/head'
+import Image from 'next/image'
 
-import styles from '@/styles/Home.module.css';
+import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -65,5 +65,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
